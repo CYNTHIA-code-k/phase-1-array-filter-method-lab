19e5f7b5cdc6b008d143b51b01c27b4ccf906f34
cynthia <cyeeyearn@gmail.com>
2024-04-07 18:09:01 +0300

    Array Filter

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const driverObjects = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' }
+];
+
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+function matchName(driverObjects, query) {
+  return driverObjects.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
+//
+console.log(findMatching(drivers, 'Bobby')); // Output: ['Bobby', 'Bobby']
+console.log(fuzzyMatch(drivers, 'Sa'));      // Output: ['Sammy', 'Sarah']
+console.log(matchName(driverObjects, 'Sally')); // Output: [{ name: 'Sally', hometown: 'Cleveland' }]
